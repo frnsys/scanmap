bb086138dce4984e8c357588154b58be943a5fe6
Francis Tseng <f+accounts@frnsys.com>
2020-06-03 11:46:53 -0400

    backend cleanup

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import json
 import yaml
 import config
 import requests
-from functools import wraps
 from datetime import datetime, timezone
 from flask import Flask, abort, request, render_template, jsonify
 
@@ -56,9 +55,10 @@ def log(location):
         auth = request.headers.get('X-AUTH')
         if not check_key(auth, location):
             abort(401)
+
         data = request.get_json()
-        data['timestamp'] = datetime.utcnow().replace(tzinfo=timezone.utc).timestamp()
         data['submitter'] = auth
+        data['timestamp'] = datetime.utcnow().replace(tzinfo=timezone.utc).timestamp()
         with open(conf['DB_FILE'], 'a') as f:
             f.write(json.dumps(data) + '\n')
         return jsonify(success=True)
@@ -70,7 +70,6 @@ def log(location):
             logs = []
         return jsonify(logs=[json.loads(l) for l in logs if l])
 
-
 @app.route('/<location>/location', methods=['POST'])
 def query_location(location):
     conf = get_conf(location)
@@ -78,15 +77,11 @@ def query_location(location):
     if not check_key(auth, location):
         abort(401)
     data = request.get_json()
-    query = data['query']
-    results = google_search_places(query, conf)
-    results = [r for r in results if conf['SEARCH']['FILTER'] in r['address']]
+    results = google_search_places(data['query'], conf)
     return jsonify(results=results)
-
 
 def google_search_places(query, conf):
     """Reference: <https://developers.google.com/places/web-service/search?hl=ru#nearby-search-and-text-search-responses>"""
-    url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
     params = {
         'key': config.GOOGLE_PLACES_API_KEY,
         'query': query,
@@ -95,26 +90,25 @@ def google_search_places(query, conf):
         'location': conf['SEARCH']['CENTER'],
         'radius': conf['SEARCH']['RADIUS']
     }
-    resp = requests.get(url, params=params)
+
+    # Not paginating results because we
+    # only will show a few
+    resp = requests.get(
+            'https://maps.googleapis.com/maps/api/place/textsearch/json',
+            params=params)
     data = resp.json()
     results = data['results']
-    while 'next_page_token' in data:
-        page_token = data['next_page_token']
-        params = {
-            'key': config.GOOGLE_PLACES_API_KEY,
-            'pagetoken': page_token,
-        }
-        resp = requests.get(url, params=params)
-        data = resp.json()
-        results += data['results']
+
+    # Hard filter to keep search results relevant to the region
+    results = [r for r in results
+                if conf['SEARCH']['FILTER'] in r['formatted_address']]
+
     return [{
-        'source': 'google',
         'name': r['name'],
         'coordinates': [
             r['geometry']['location']['lat'],
             r['geometry']['location']['lng']
-        ],
-        'address': r['formatted_address']
+        ]
     } for r in results]
 
 
