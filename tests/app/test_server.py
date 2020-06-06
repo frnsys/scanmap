d4d5454fb638d239edab8770fda0ace58b2b202b
Ferris Tseng <ferris@navapbc.com>
2020-06-06 12:45:27 -0400

    more tests

--- tests/app/test_server.py
+++ tests/app/test_server.py
@@ -1,27 +1,43 @@
 from app.db import Database
 from app.keys import KeyRing
 from datetime import datetime, timezone
+import json
 import pytest
-import server
+import requests
 import tempfile
+
+class MockResponse:
+    def __init__(self, response_file):
+        self.response_path = 'tests/app/data/responses/{}.json'.format(response_file)
+
+    def json(self):
+        with open(self.response_path, mode='r') as f:
+            return json.load(f)
 
 @pytest.fixture()
 def client(monkeypatch):
-    with monkeypatch.context() as test_ctxt:
-        with tempfile.NamedTemporaryFile() as keyf:
-            # Copy over the test keys to the temporary key file
-            with open('tests/app/data/keys.yml', mode='rb') as testkeyf:
-                keyf.write(testkeyf.read())
-                keyf.flush()
+    with tempfile.NamedTemporaryFile() as keyf:
+        # Copy over the test keys to the temporary key file
+        with open('tests/app/data/keys.yml', mode='rb') as testkeyf:
+            keyf.write(testkeyf.read())
+            keyf.flush()
 
-            with tempfile.NamedTemporaryFile() as dbf:
-                # Stub key ring and database
-                test_ctxt.setattr(server, 'kr', KeyRing(keyf.name))
-                test_ctxt.setattr(server, 'db', Database(dbf.name))
+        with tempfile.NamedTemporaryFile() as dbf:
+            import server
 
-                server.app.config['TESTING'] = True
+            # Stub key ring and database
+            monkeypatch.setattr(server, 'kr', KeyRing(keyf.name))
+            monkeypatch.setattr(server, 'db', Database(dbf.name))
 
-                yield server.app.test_client()
+            server.app.config['TESTING'] = True
+
+            yield server.app.test_client()
+
+# ---------------------
+# ---------------------
+# ---  GET /version ---
+# ---------------------
+# ---------------------
 
 def test_get_version(client):
     """Tests version route to ensure json body is expected"""
@@ -35,6 +51,12 @@ def test_get_version(client):
     # Sanity check to ensure the config that's being used is
     # the one defined in tests/app/config.py.
     assert(version_response.get_json()['version'] == 'TEST')
+
+# -------------------------
+# -------------------------
+# ---  GET /<location>/ ---
+# -------------------------
+# -------------------------
 
 def test_get_map_location(client):
     """Tests getting a known location"""
@@ -50,6 +72,12 @@ def test_get_unknown_map_location(client):
 
     assert(map_response.status_code == 404)
 
+# -----------------------------
+# -----------------------------
+# ---  GET /<location>/cams ---
+# -----------------------------
+# -----------------------------
+
 def test_get_cams(client):
     """Tests getting cams"""
 
@@ -57,12 +85,102 @@ def test_get_cams(client):
 
     assert(cams_response.status_code == 200)
 
-def test_get_unknown_cams_location(client):
+def test_get_cams_unknown_location(client):
     """Tests getting cams from an unknown location"""
 
     cams_response = client.get('/YN/cams')
 
     assert(cams_response.status_code == 404)
+
+# ----------------------------------
+# ----------------------------------
+# ---  POST /<location>/location ---
+# ----------------------------------
+# ----------------------------------
+
+def test_get_location_unauthorized(client):
+    """Tests searching for location with an unauthorized key"""
+
+    location_response = client.post(
+        '/NY/location',
+        headers={'X-AUTH': 'BOGUS'},
+        json={'query': 'barclays center'})
+
+    assert(location_response.status_code == 401)
+
+def test_get_location_barclays_center(client, monkeypatch):
+    """Tests searching for address of a business"""
+
+    monkeypatch.setattr(
+        requests,
+        'get',
+        lambda *args, **kwargs: MockResponse('google_places_ok_0_address'))
+
+    location_response = client.post(
+        '/NY/location',
+        headers={'X-AUTH': 'WRITE'},
+        json={'query': 'barclays center'})
+
+    assert(location_response.status_code == 200)
+    assert(any(location_response.get_json()['results']))
+
+def test_get_location_cross_streets(client, monkeypatch):
+    """Tests searching for a cross streets"""
+
+    monkeypatch.setattr(
+        requests,
+        'get',
+        lambda *args, **kwargs: MockResponse('google_places_ok_1_cross'))
+
+    location_response = client.post(
+        '/NY/location',
+        headers={'X-AUTH': 'WRITE'},
+        json={'query': 'bergen st and 4th av'})
+
+    assert(location_response.status_code == 200)
+    assert(any(location_response.get_json()['results']))
+
+def test_get_location_ambiguous(client, monkeypatch):
+    """Tests searching for a term that has multiple results"""
+
+    monkeypatch.setattr(
+        requests,
+        'get',
+        lambda *args, **kwargs: MockResponse('google_places_ok_2_ambiguous'))
+
+    location_response = client.post(
+        '/NY/location',
+        headers={'X-AUTH': 'WRITE'},
+        json={'query': 'bergen'})
+
+    assert(location_response.status_code == 200)
+    assert(len(location_response.get_json()['results']) == 4)
+
+# ------------------------------
+# ------------------------------
+# ---  GET /<location>/panel ---
+# ------------------------------
+# ------------------------------
+
+def test_get_panel_ok(client):
+    """Tests getting a panel"""
+
+    panel_response = client.get('/NY/cams')
+
+    assert(panel_response.status_code == 200)
+
+def test_get_panel_unknown_location(client):
+    """Tests getting a panel from an unknown location"""
+
+    panel_response = client.get('/YN/cams')
+
+    assert(panel_response.status_code == 404)
+
+# -----------------------------------
+# -----------------------------------
+# ---  GET, POST /<location>/keys ---
+# -----------------------------------
+# -----------------------------------
 
 def test_keys_no_auth(client):
     """Tests keys route with no auth"""
@@ -84,6 +202,58 @@ def test_keys_prime_get(client):
     keys_response = client.get('/NY/keys', headers={'X-AUTH': 'PRIME'})
 
     assert(keys_response.status_code == 200)
+
+def test_keys_new_key_unauthorized(client):
+    """Tests adding a new key with an unauthorized key"""
+
+    keys_response = client.post(
+        '/NY/keys',
+        headers={'X-AUTH': 'WRITE'},
+        json={'action': 'create'})
+
+    assert(keys_response.status_code == 401)
+
+def test_keys_new_key_ok(client):
+    """Tests add a key with a prime key"""
+
+    import server
+
+    keys_response = client.post(
+        '/NY/keys',
+        headers={'X-AUTH': 'PRIME'},
+        json={'action': 'create'})
+
+    assert(keys_response.status_code == 200)
+    assert(len(server.kr.get_keys('NY')['write']) == 2)
+
+def test_keys_revoke_key_unauthorized(client):
+    """Tests revoking a key with an unauthorized key"""
+
+    keys_response = client.post(
+        '/NY/keys',
+        headers={'X-AUTH': 'WRITE'},
+        json={'action': 'revoke', 'key': 'WRITE'})
+
+    assert(keys_response.status_code == 401)
+
+def test_keys_revoke_key_ok(client):
+    """Tests revoking a key with a prime key"""
+
+    import server
+
+    keys_response = client.post(
+        '/NY/keys',
+        headers={'X-AUTH': 'PRIME'},
+        json={'action': 'revoke', 'key': 'WRITE'})
+
+    assert(keys_response.status_code == 200)
+    assert(len(server.kr.get_keys('NY')['write']) == 0)
+
+# -----------------------------------
+# -----------------------------------
+# ---  POST /<location>/checkauth ---
+# -----------------------------------
+# -----------------------------------
 
 def test_check_auth_ok_key(client):
     """Tests check auth with a good key and location"""
